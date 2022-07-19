34fbfe4b9f82fd3bb6dc165ba287b319090aaafd
Talipov S.N <proffix4@gmail.com>
2022-07-19 12:48:14 +0600

    Add files via upload

--- jjar.bat
+++ jjar.bat
@@ -1,3 +1,3 @@
 @echo off
-chcp 655001 > nul
+chcp 65001 > nul
 java -Dsun.stdout.encoding=UTF-8 -Dsun.stdin.encoding=UTF-8 -Dfile.encoding=UTF-8 -Dsun.jnu.encoding=UTF-8 -Dconsole.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -jar %1
