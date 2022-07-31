5241b7873f081bd8b6ad36fad25ec7d9a1c81c19
Talipov S.N <proffix4@gmail.com>
2022-07-31 15:16:00 +0600

    Delete Рабочая нагрузка Linux.bat

--- Рабочая нагрузка Linux.bat
+++
@@ -1,3 +0,0 @@
-﻿rem https://docs.microsoft.com/ru-ru/cpp/linux/download-install-and-setup-the-linux-development-workload?view=msvc-170
-wsl sudo apt-get install openssh-server g++ gdb make ninja-build rsync zip
-wsl sudo service ssh start
