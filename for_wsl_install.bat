5d1ee086f01cce27bd92b31bf279d1d1cc44c41c
Talipov S.N <proffix4@gmail.com>
2022-08-14 20:26:18 +0600

    Add files via upload

--- for_wsl_install.bat
+++ for_wsl_install.bat
@@ -1,1 +1,4 @@
-wsl sudo apt install -y wslu mono-complete mono-vbnc
+wsl sudo apt update -y
+wsl sudo apt upgrade -y
+wsl sudo apt install -y wine winetricks wslu mono-complete mono-vbnc
+wsl sudo apt autoremove -y
